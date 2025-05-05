67eb4546fbd3876adfc34b1e00848e64eae471f8
MayerlinLemosBedoya <maleiquianchico@hotmail.com>
2025-05-05 11:58:06 -0500

    Inventario

---
+++ routes/holaMundo. routes.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const router = express.Router()
+const holaController= require("../controllers/holaMundo.controllers")
+
+router.get("/",holaController.holaMundo)
+router.get("/esteban",(req,res)=>{
+    res.send("hola esteban")
+})
+router.get("/esteban",(req,res)=>{
+    res.send("hola esteban")
+})
+
+module.exports =router
